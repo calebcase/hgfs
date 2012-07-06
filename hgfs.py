263d748d6b273d35f52b1c55fa13db3e81c4aa5e
Caleb Case <calebcase@gmail.com>
2012-07-05 23:38:44 -0400

    Cleaning up dispatch and attribute handling.

--- hgfs.py
+++ hgfs.py
@@ -44,8 +44,7 @@ class HgFS(LoggingMixIn, Operations):
 
     def destroy(self, path):
         try:
-            dispatch(request(['--cwd', self.tmp, 'add', self.tmp]))
-            dispatch(request(['--cwd', self.tmp, 'commit', '-m \"cruft\"']))
+            dispatch(request(['--cwd', self.tmp, 'commit', '-A', '-m \"cruft\"']))
             dispatch(request(['--cwd', self.tmp, 'push']))
         finally:
             shutil.rmtree(self.tmp)
@@ -55,8 +54,6 @@ class HgFS(LoggingMixIn, Operations):
 
         try:
             os.mkdir(ahgfs)
-            dispatch(request(['--cwd', self.tmp, 'add', ahgfs]))
-            dispatch(request(['--cwd', self.tmp, 'commit', '-m \"Adding .hgfs\"']))
         except Exception, e:
             self.log.debug("Failed to create .hgfs.")
             pass
@@ -74,8 +71,9 @@ class HgFS(LoggingMixIn, Operations):
                     os.chown(atarget, attrs['st_uid'], attrs['st_gid'])
 
     def __save_attributes(self, path, msg):
+        _path = path[1:]
         try:
-            ahgfs_path = os.path.join(self.tmp, '.hgfs', os.path.dirname(path[1:]))
+            ahgfs_path = os.path.join(self.tmp, '.hgfs', os.path.dirname(_path))
             os.makedirs(ahgfs_path)
         except OSError, e:
             if e.errno == errno.EEXIST:
@@ -83,57 +81,66 @@ class HgFS(LoggingMixIn, Operations):
             else:
                 raise
 
-        apath = os.path.join(self.tmp, path[1:])
+        apath = os.path.join(self.tmp, _path)
         st = os.stat(apath)
 
         attributes = dict((key, getattr(st, key)) for key in self.ATTRS)
 
-        modpath = os.path.join(self.tmp, '.hgfs', path[1:] + '.attr')
+        modpath = os.path.join(self.tmp, '.hgfs', _path + '.attr')
         with open(modpath, 'w+') as f:
             f.write(json.dumps(attributes, sort_keys=True, indent=2))
 
         ahgfs = os.path.join(self.tmp, '.hgfs')
 
-        dispatch(request(['--cwd', self.tmp, 'add', ahgfs]))
-        dispatch(request(['--cwd', self.tmp, 'commit', '-m', msg]))
+        dispatch(request(['--cwd', self.tmp, 'commit', '-A', '-m', msg, '.hgfs', str(_path)]))
         dispatch(request(['--cwd', self.tmp, 'push']))
 
     def chmod(self, path, mode):
-        apath = os.path.join(self.tmp, path[1:])
+        _path = path[1:]
+        apath = os.path.join(self.tmp, _path)
         status = os.chmod(apath, mode)
 
-        self.__save_attributes(path, "chmod: %s %o" % (path[1:], mode))
+        self.__save_attributes(path, "chmod: %s %o" % (_path, mode))
 
         return status
 
     def chown(self, path, uid, gid):
-        apath = os.path.join(self.tmp, path[1:])
+        _path = path[1:]
+        apath = os.path.join(self.tmp, _path)
         status = os.chown(apath, uid, gid)
 
-        self.__save_attributes(path, "chown: %s %d %d" % (path[1:], uid, gid))
+        self.__save_attributes(path, "chown: %s %d %d" % (_path, uid, gid))
 
         return status
 
     def create(self, path, mode, fi=None):
-        apath = os.path.join(self.tmp, path[1:])
+        _path = path[1:]
+        apath = os.path.join(self.tmp, _path)
 
         with open(apath, 'w+', mode) as f:
             pass
 
-        dispatch(request(['--cwd', self.tmp, 'add', apath]))
-        dispatch(request(['--cwd', self.tmp, 'commit', '-m \"create: %s\"' % path[1:]]))
-        dispatch(request(['--cwd', self.tmp, 'push']))
+
+        self.__save_attributes(path, "create: %s %o" % (_path, mode))
 
         return 0
 
     def getattr(self, path, fh=None):
+        dispatch(request(['--cwd', self.tmp, 'pull', '-u']))
+
         apath = os.path.join(self.tmp, path[1:])
         st = os.stat(apath)
         return dict((key, getattr(st, key)) for key in self.ATTRS)
 
     def mkdir(self, path, mode):
-        apath = os.path.join(self.tmp, path[1:])
-        return os.mkdir(apath, mode)
+        _path = path[1:]
+        apath = os.path.join(self.tmp, _path)
+
+        status = os.mkdir(apath, mode)
+
+        self.__save_attributes(path, "mkdir: %s %o" % (_path, mode))
+
+        return status
 
     def read(self, path, size, offset, fh):
         dispatch(request(['--cwd', self.tmp, 'pull', '-u']))
@@ -144,83 +151,115 @@ class HgFS(LoggingMixIn, Operations):
         return data
 
     def readdir(self, path, fh):
+        dispatch(request(['--cwd', self.tmp, 'pull', '-u']))
+
         apath = os.path.join(self.tmp, path[1:])
         paths = os.listdir(apath)
         clean = []
 
         for path in paths:
-            if not path.startswith('.hg'):
+            if path != '.hg' and path != '.hgfs':
                 clean.append(path)
 
         return clean
 
     def readlink(self, path):
+        dispatch(request(['--cwd', self.tmp, 'pull', '-u']))
+
         apath = os.path.join(self.tmp, path[1:])
         return os.readlink(apath)
 
     def rename(self, old, new):
-        aold = os.path.join(self.tmp, old[1:])
-        anew = os.path.join(self.tmp, new[1:])
+        _old = old[1:]
+        _new = new[1:]
 
-        dispatch(request(['--cwd', self.tmp, 'mv', aold, anew]))
-        dispatch(request(['--cwd', self.tmp, 'commit', '-m \"rename: %s -> %s\"' % (old[1:], new[1:])]))
+        dispatch(request(['--cwd', self.tmp, 'mv', _old, _new]))
+        dispatch(request(['--cwd', self.tmp, 'mv', os.path.join('.hgfs', _old + '.attr'), os.path.join('.hgfs', _new + '.attr')]))
+        dispatch(request(['--cwd', self.tmp, 'commit', '-m', "rename: %s -> %s" % (_old, _new), str(_old), str(_new)]))
         dispatch(request(['--cwd', self.tmp, 'push']))
 
         return 0
 
     def rmdir(self, path):
-        apath = os.path.join(self.tmp, path[1:])
+        _path = path[1:]
+        apath = os.path.join(self.tmp, _path)
+        dpath = os.path.join(self.tmp, '.hgfs', _path)
+        hpath = os.path.join(self.tmp, '.hgfs', _path + '.attr')
 
-        dispatch(request(['--cwd', self.tmp, 'rm', apath]))
-        dispatch(request(['--cwd', self.tmp, 'commit', '-m \"rmdir: %s\"' % (path[1:])]))
-        dispatch(request(['--cwd', self.tmp, 'push']))
+        try:
+            status = os.rmdir(dpath)
+        except:
+            pass
 
-        return 0
+        status = os.rmdir(apath)
 
-    def symlink(self, target, source):
-        asource = os.path.join(self.tmp, source[1:])
-        atarget = os.path.join(self.tmp, target[1:])
+        try:
+            status = os.unlink(hpath)
+        except:
+            pass
 
-        dispatch(request(['--cwd', self.tmp, 'add', asource]))
-        dispatch(request(['--cwd', self.tmp, 'add', atarget]))
-        dispatch(request(['--cwd', self.tmp, 'commit', '-m \"symlink: %s -> %s\"' % (source[1:], target[1:])]))
-        dispatch(request(['--cwd', self.tmp, 'push']))
-
-        return os.symlink(asource, atarget)
-
-    def truncate(self, path, length, fh=None):
-        apath = os.path.join(self.tmp, path[1:])
-        status = 0
-        with open(apath, 'r+') as f:
-            status = f.truncate(length)
-
-        dispatch(request(['--cwd', self.tmp, 'add', apath]))
-        dispatch(request(['--cwd', self.tmp, 'commit', '-m \"truncate: %s\"' % (path[1:])]))
+        dispatch(request(['--cwd', self.tmp, 'commit', '-A', '-m', "rmdir: %s" % (_path), str(_path)]))
         dispatch(request(['--cwd', self.tmp, 'push']))
 
         return status
 
     def unlink(self, path):
-        apath = os.path.join(self.tmp, path[1:])
+        _path = path[1:]
+        apath = os.path.join(self.tmp, _path)
+        hpath = os.path.join(self.tmp, '.hgfs', _path + '.attr')
 
-        dispatch(request(['--cwd', self.tmp, 'rm', apath]))
-        dispatch(request(['--cwd', self.tmp, 'commit', '-m \"unlink: %s\"' % (path[1:])]))
+        status = os.unlink(apath)
+
+        try:
+            status = os.unlink(hpath)
+        except:
+            pass
+
+        dispatch(request(['--cwd', self.tmp, 'commit', '-A', '-m', "unlink: %s" % (_path), str(_path)]))
         dispatch(request(['--cwd', self.tmp, 'push']))
 
-        return 0
+        return status
+
+    def symlink(self, target, source):
+        _source = source[1:]
+        _target = target[1:]
+
+        asource = os.path.join(self.tmp, _source)
+        atarget = os.path.join(self.tmp, _target)
+
+        status = os.symlink(source, atarget)
+
+        dispatch(request(['--cwd', self.tmp, 'commit', '-A', '-m', "symlink: %s -> %s" % (source, _target), str(source), str(_target)]))
+        dispatch(request(['--cwd', self.tmp, 'push']))
+
+        return status
+
+    def truncate(self, path, length, fh=None):
+        _path = path[1:]
+        apath = os.path.join(self.tmp, _path)
+
+        status = 0
+        with open(apath, 'r+') as f:
+            status = f.truncate(length)
+
+        dispatch(request(['--cwd', self.tmp, 'commit', '-A', '-m', "truncate: %s" % (_path), str(_path)]))
+        dispatch(request(['--cwd', self.tmp, 'push']))
+
+        return status
 
     def utimens(self, path, times=None):
         apath = os.path.join(self.tmp, path[1:])
         return os.utime(apath, times)
 
     def write(self, path, data, offset, fh):
-        apath = os.path.join(self.tmp, path[1:])
+        _path = path[1:]
+        apath = os.path.join(self.tmp, _path)
+
         with open(apath, 'r+') as f:
             f.seek(offset, 0)
             f.write(data)
 
-        dispatch(request(['--cwd', self.tmp, 'add', apath]))
-        dispatch(request(['--cwd', self.tmp, 'commit', '-m \"write: %s\"' % (path[1:])]))
+        dispatch(request(['--cwd', self.tmp, 'commit', '-A', '-m', "write: %s" % (_path), str(_path)]))
         dispatch(request(['--cwd', self.tmp, 'push']))
 
         return len(data)
